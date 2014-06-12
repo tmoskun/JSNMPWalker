ff8dc4b049124f0c527e277a472b7480ebd47c87
tmoskun <tanyamoskun@gmail.com>
2014-06-12 13:37:37 -0700

    rebuilt packages

--- installers/mac/JSNMPWalker-v2.0.mpkg/Contents/Resources/checkjava.sh
+++ installers/mac/JSNMPWalker-v2.0.mpkg/Contents/Resources/checkjava.sh
@@ -1,18 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-# preinstall
-# 
-#
-# Created by T. Moskun on 10/23/13.
-# Copyright 2013 ezcode. All rights reserved.
-java=$(which java | sed 's|/bin/.*||')
-if [ "x$java" = "x" ];then
-	echo "Java is not installed"
-	$(exit 1)
-	java_version=$(java -version 2>&1 |head -n 1 | cut -d\" -f 2 | sed -E 's|^([0-9])\.([0-9])(\..+)?$|\2|' | bc)
-	if [ $java_version -lt 6 ];then
-		echo "Java 7 is not installed"
-		$(exit 1)
-	fi
-fi
-$(exit 0)
+version=$(java -version 2>&1 |head -n 1 | cut -d\" -f 2 | sed -E 's|^([0-9])\.([0-9])(\..+)?$|\2|' | bc)
+$(exit $version)
